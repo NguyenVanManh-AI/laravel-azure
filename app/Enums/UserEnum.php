044fffaa33b2abc15f715651f30e43776942f5f3
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2023-10-27 06:32:45 +0700

    update4

--- app/Enums/UserEnum.php
+++ app/Enums/UserEnum.php
@@ -8,7 +8,7 @@ class UserEnum extends BaseEnum
 
     public const PATH_FILE_DB = 'storage/Blog/image/avatars/';
 
-    public const DOMAIN_PATH = 'http://localhost:99/';
+    public const DOMAIN_PATH = 'https://vanmanh.azurewebsites.net/';
 
     public const DOMAIN_CLIENT = 'http://localhost:4200/';
     // public const DOMAIN_PATH = 'http://192.168.11.195:8000/';
