2c5dba5e0ef27372ac8375fc0a0905bfe0e07de8
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2023-10-29 11:49:51 +0700

    update

--- resources/views/emails/send_code.blade.php
+++ resources/views/emails/send_code.blade.php
@@ -4,11 +4,19 @@
 <head>
     <title>Reset Password</title>
 </head>
-
+<style>
+    p {
+        color: #007bff;
+    }
+    p:hover {
+        text-decoration: underline;
+        color: #0059b9
+    }
+</style>
 <body>
     <h1>Reset Password from System</h1>
     <h2>Here is the email to reset the password. Please click the link here. <br>
-        <a href="{{ $url }}">{{ $url }}</a>
+        <p>{{ $url }}</p>
     </h2>
 </body>
 
--- resources/views/emails/verify_email.blade.php
+++ resources/views/emails/verify_email.blade.php
@@ -4,11 +4,20 @@
 <head>
     <title>Verify Email</title>
 </head>
-
+<style>
+    p {
+        color: #007bff;
+    }
+    p:hover {
+        text-decoration: underline;
+        color: #0059b9
+    }
+</style>
 <body>
     <h1>Verify Email from System</h1>
     <h2>Click on this link to confirm your email. Only when the email is confirmed can you log in. <br>
-        <a href="{{ $url }}">{{ $url }}</a>
+        {{-- <a href="{{ $url }}">{{ $url }}</a> --}}
+        <p>{{ $url }}</p>
     </h2>
 </body>
 
