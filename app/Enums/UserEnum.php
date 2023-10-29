02ef5b6bf7539fae96ab7216da4dd7de02cb0658
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2023-10-29 11:24:02 +0700

    update

--- app/Enums/UserEnum.php
+++ app/Enums/UserEnum.php
@@ -9,8 +9,8 @@ class UserEnum extends BaseEnum
     public const PATH_FILE_DB = 'storage/Blog/image/avatars/';
 
     // public const DOMAIN_PATH = 'http://localhost:99/'; // docker 
-    public const DOMAIN_PATH = 'http://localhost:8000/'; // laragon 
+    // public const DOMAIN_PATH = 'http://localhost:8000/'; // laragon 
 
     public const DOMAIN_CLIENT = 'http://localhost:4200/';
-    // public const DOMAIN_PATH = 'https://vanmanh.azurewebsites.net/'; // azure 
+    public const DOMAIN_PATH = 'https://vanmanh.azurewebsites.net/'; // azure 
 }
