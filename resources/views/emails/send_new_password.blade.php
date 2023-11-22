eb64b66fd5a00111bb46dc82f7ce780a548415cf
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2023-11-22 18:34:57 +0700

    update mail content

--- resources/views/emails/send_new_password.blade.php
+++ resources/views/emails/send_new_password.blade.php
@@ -1,15 +1,66 @@
 <!DOCTYPE html>
 <html>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <head>
-    <title>Thông tin tài khoản</title>
+    <title>Đặt lại mật khẩu</title>
 </head>
 
 <body>
-    <h3>Hãy sử dụng tài khoản này để đăng nhập vào hệ thống và đổi mật khẩu !<br>
-        <h4>Email : {{$email}}</h4>
-        <h4>Password : {{$new_password}}</h4>
-    </h3>
+    <div style="
+    background-color: rgb(240, 255, 240);
+    justify-content: center;
+    border: 2px solid silver;
+    margin-bottom: 3px;
+    padding: 10px;
+    text-align: center;
+    ">
+        <p style="font-style: italic;margin: 0;margin-bottom: 10px;">Xin chào bạn ! Bạn có một thông báo đến từ hệ thống Elister Health Care !</p>
+        <p style="margin: 0;margin-bottom: 5px;">{{ now() }}</p>
+    </div>
+
+    <div style="background-color: #007eff1c;padding: 20px;border: 2px dashed #007bff;">
+        <p>
+            <p><h4>Hãy sử dụng tài khoản này để đăng nhập vào hệ thống và đổi mật khẩu !</h4></p>
+            <p>Email : <span style="font-weight: bold; ">{{$email}}</span> </p>
+            <p>Password : <span style="font-weight: bold; ">{{$new_password}}</span> </p>
+        </p>
+    </div>
+
+    <div style="
+    background-color: rgb(240, 255, 240);
+    justify-content: center;
+    border: 2px solid silver;
+    margin-top: 3px;
+    padding: 20px;
+    text-align: center;
+    ">
+    <p style="margin: 0;margin-bottom: 10px;">Nếu bạn có bất kỳ câu hỏi nào, vui lòng gửi email cho chúng tôi theo địa chỉ <a style="font-weight: bold;color: rgb(0, 123, 255);" href="mailto:help@elisterhealthcare.com">help@elisterhealthcare.com</a> hoặc truy cập <a style="font-weight: bold;color: rgb(0, 123, 255);" href="FAQs">FAQs</a> của chúng tôi.</p>
+    <p style="margin: 0;margin-bottom: 10px;">Bạn cũng có thể trò chuyện với người thật trong giờ làm việc của chúng tôi. Họ có thể trả lời các câu hỏi về tài khoản của bạn hoặc giúp bạn giải đáp các thắt mắc khác.</p>
+    
+    <div style="width: 30%;height: 2px;background-color: silver;margin: auto;"></div>
+    <div style="margin-top: 10px;margin-bottom: 5px;font-size: 30px;">
+        <span style="margin-right: 5px;margin-left: 5px;cursor: pointer;"><a href="https://www.facebook.com/"><img style="width: 40px" src="https://lh3.googleusercontent.com/drive-viewer/AK7aPaDgJ5iImE-1lv68eupQbVbH4YhslK_VmkcmwuEnQtyG-uunjT_uy3zF45o30jC_df_33PzOgFcPxtEyQskDM3s9qTLL2A=w1919-h878" alt=""></a></span>
+        <span style="margin-right: 5px;margin-left: 5px;cursor: pointer;"><a href="https://www.instagram.com/"><img style="width: 40px" src="https://lh3.googleusercontent.com/drive-viewer/AK7aPaBesoQesgiHEsfRstJV2P90NaCbNvb2R2USBxjzMVdiVXLQfmtNCpQ0kHAXFU9K8FFI9B8cQKxi-LlGcasOl5teonDnBg=w1919-h878" alt=""></a> </span>
+        <span style="margin-right: 5px;margin-left: 5px;cursor: pointer;"><a href="https://twitter.com/"><img style="width: 45px" src="https://lh3.googleusercontent.com/drive-viewer/AK7aPaAEf7c2umur0Ld2p-5PJdxYNbzbHCvYEEu_jfVp4JqtnykaB8IjefMVW83szvmflsLcFegFR40rxbKJlUZODZKLQMyF=w1919-h878" alt=""></a> </span>
+        <span style="margin-right: 5px;margin-left: 5px;cursor: pointer;"><a href="https://www.youtube.com/"><img style="width: 50px" src="https://lh3.googleusercontent.com/drive-viewer/AK7aPaB5PRRGQGZRuiVsiMGK6XNA5c1vIgenY9aFYNL3JPpcnk-bzX-z0A4AZwiUnG8vZpWaiXTvKp-8M-6Et1OuNTLXUpxQBQ=w1919-h878" alt=""></a></span>
+    </div>
+    <p style="margin: 0;margin-bottom: 10px;">Bạn đã nhận được email này với tư cách là người dùng đăng ký của <a style="font-weight: bold;color: rgb(0, 123, 255);" href="https://react-vercel-95yac2b5v-vanmanh-react.vercel.app/"><strong>Elisterhealthcare.com</strong></a></p>
+    <p style="margin: 0;margin-bottom: 10px;">Bạn có thể hủy đăng ký nhận những email này tại đây.</p>
+    <p style="margin: 0;margin-bottom: 10px;">54 Nguyễn Lương Bằng, phường Hòa Khánh Bắc, Quận Liên Chiểu , Đà Nẵng , Việt Nam</p>
+    <p style="margin: 0;margin-bottom: 10px;">Số điện thoại công ty : <a style="font-weight: bold;color: rgb(0, 123, 255);" href="tel:+01236000333">01236000333</a></p>
+    <p style="font-weight: bold;margin: 0;margin-bottom: 10px;">© Elisterhealthcare, Inc</p>
+    <div id="div_img">
+        <a style="text-decoration: none;" href="https://react-vercel-95yac2b5v-vanmanh-react.vercel.app/">
+            <img style="width: 150px;margin-left: 10px;margin-right: 10px;" src="https://lh3.googleusercontent.com/drive-viewer/AK7aPaAVVzCYweZdp_cmfJXdPUnXN-I4b7eF3sZnJHP7-kagWCYXwqz7QyKK0IgdekcwG0kvjbhuyrTeTtAeRR0D-CHwc3SiUg=w1919-h878" alt="">
+        </a>
+        <a style="text-decoration: none;" href="https://react-vercel-95yac2b5v-vanmanh-react.vercel.app/">
+            <img style="width: 150px;margin-left: 10px;margin-right: 10px;" src="https://lh3.googleusercontent.com/drive-viewer/AK7aPaAVVzCYweZdp_cmfJXdPUnXN-I4b7eF3sZnJHP7-kagWCYXwqz7QyKK0IgdekcwG0kvjbhuyrTeTtAeRR0D-CHwc3SiUg=w1919-h878" alt="">
+        </a>
+        <a style="text-decoration: none;" href="https://react-vercel-95yac2b5v-vanmanh-react.vercel.app/">
+            <img style="width: 150px;margin-left: 10px;margin-right: 10px;" src="https://lh3.googleusercontent.com/drive-viewer/AK7aPaAVVzCYweZdp_cmfJXdPUnXN-I4b7eF3sZnJHP7-kagWCYXwqz7QyKK0IgdekcwG0kvjbhuyrTeTtAeRR0D-CHwc3SiUg=w1919-h878" alt="">
+        </a>
+    </div>
+</div>
 </body>
 
 </html>
